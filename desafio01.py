40c211dbeebcc36fe6300a3ea4c1baccb8455ae5
falpz <50431355+falpz@users.noreply.github.com>
2019-07-17 00:12:58 -0300

    Add files via upload

---
+++ desafio01.py
@@ -0,0 +1,9 @@
+print('Vou te fazer algumas perguntas tudo bem? Estou testando esse script!')
+nome = input('Olá, Qual seu nome? ')
+print('Muito prazer,',nome,'!')
+idade = input('Qual a sua idade? ')
+print('Está novo ainda amigo(a)!')
+cidade = input('Onde você mora? ')
+print('Já viajei para essa cidade!')
+print ('Obrigado por responder e me ajudar a testar esse script!')
+
---
+++ desafio02.py
@@ -0,0 +1,7 @@
+print ('Chegou a hora de testar minha capacidade de cálculos!')
+num1=input ('Por favor, digite um número = ')
+num2=input ('Agora digite outro número = ')
+soma= int(num1)+int(num2)
+print ('De acordo com os meus cáculos, o resultado é:',soma,'.')
+
+
